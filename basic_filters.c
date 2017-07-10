917093d8e24d960021567ddaa5649126ae3bbeaa
Nikolay Marchuk <marchuk.nikolay.a@gmail.com>
2017-07-10 19:32:39 +0700

    Improve fd filtering
    * pathtrace.c (match_fd_common, pathtrace_match_set): Move fd matching to
    separate function.
    * filter.h (match_fd_common): Add new declaration.
    * basic_filters.c (run_fd_filter): Use match_fd_common for fd filter.

--- basic_filters.c
+++ basic_filters.c
@@ -27,8 +27,9 @@
  */
 
 #include "defs.h"
-#include "filter.h"
 #include <regex.h>
+#include "filter.h"
+#include "syscall.h"
 
 typedef unsigned int number_slot_t;
 #define BITS_PER_SLOT (sizeof(number_slot_t) * 8)
@@ -402,15 +403,31 @@ parse_fd_filter(const char *str)
 	return set;
 }
 
+static bool
+is_fd_in_set(struct tcb *tcp, int fd, void *data) {
+	struct number_set *set = data;
+
+	if (fd < 0)
+		return set->not;
+	return is_number_in_set(fd, set);
+}
+
 bool
 run_fd_filter(struct tcb *tcp, void *_priv_data)
 {
-	int fd = tcp->u_arg[0];
 	struct number_set *set = _priv_data;
 
-	if (fd < 0)
-		return false;
-	return is_number_in_set(fd, set);
+	/*
+	 * mq_timedsend and mq_timedreceive are not marked as descriptor
+	 * syscalls, but they can be dumped with -e read/write.
+	*/
+	switch (tcp->s_ent->sen) {
+	case SEN_mq_timedsend:
+	case SEN_mq_timedreceive:
+		return is_fd_in_set(tcp, tcp->u_arg[0], set);
+	}
+
+	return match_fd_common(tcp, &is_fd_in_set, set);
 }
 
 void
